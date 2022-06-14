c5c7545a6cdfd8bd666ef19ef0abf732bd25238c
rl1987 <rl1987@users.noreply.github.com>
2022-06-14 17:03:51 +0300

    Run black to fix formatting

--- how-to-scrape-youtube-view-intensity-time-series/yt.py
+++ how-to-scrape-youtube-view-intensity-time-series/yt.py
@@ -7,6 +7,7 @@ import requests
 from lxml import html
 import js2xml
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage:")
@@ -17,28 +18,30 @@ def main():
 
     out_f = open("view_intensity.csv", "w", encoding="utf-8")
 
-    csv_writer = csv.DictWriter(out_f, fieldnames=["time", "intensity", "url"], lineterminator="\n")
+    csv_writer = csv.DictWriter(
+        out_f, fieldnames=["time", "intensity", "url"], lineterminator="\n"
+    )
     csv_writer.writeheader()
 
     resp = requests.get(url)
     print(resp.url)
 
     tree = html.fromstring(resp.text)
-    initial_data_js = tree.xpath('//script[starts-with(text(), "var ytInitialData = ")]/text()')
+    initial_data_js = tree.xpath(
+        '//script[starts-with(text(), "var ytInitialData = ")]/text()'
+    )
     initial_data_js = initial_data_js[0]
 
     parsed = js2xml.parse(initial_data_js)
 
     for hmr in parsed.xpath('//property[@name="heatMarkerRenderer"]'):
         start = hmr.xpath('.//property[@name="timeRangeStartMillis"]/number/@value')[0]
-        score = hmr.xpath('.//property[@name="heatMarkerIntensityScoreNormalized"]/number/@value')[0]
+        score = hmr.xpath(
+            './/property[@name="heatMarkerIntensityScoreNormalized"]/number/@value'
+        )[0]
         print(start, score)
 
-        csv_writer.writerow({
-            "time": start,
-            "intensity": score,
-            "url": url
-        })
+        csv_writer.writerow({"time": start, "intensity": score, "url": url})
 
     out_f.close()
 
