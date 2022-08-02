20a8e7c9e3fc5c604d9298025a2365082632e231
rl1987 <rl1987@users.noreply.github.com>
2022-08-02 17:59:58 +0300

    Add trivial example on extracting hardcoded data from JS AST

---
+++ 2022-08-02-javascript-ast-manipulation-with-babel-extracting-hardcoded-data/extract1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const consoler = require("console");
+
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+
+let js = fs.readFileSync("fb_example.js", "utf-8");
+
+const ast = parser.parse(js);
+
+var url = null;
+
+traverse(ast, {
+    ObjectProperty: function(path) {
+        if (!path.node.method && path.node.key.name == "href") {
+            url = path.node.value.value;
+        }
+    }
+});
+
+console.log(url);
---
+++ 2022-08-02-javascript-ast-manipulation-with-babel-extracting-hardcoded-data/fb_example.js
@@ -0,0 +1,4 @@
+FB.ui({
+  method: 'share',
+  href: 'https://developers.facebook.com/docs/'
+}, function(response){});
