9f59a095205c92eb8733f4771f266fc38cf79650
rl1987 <rl1987@users.noreply.github.com>
2022-12-06 18:25:56 +0800

    Fix screenshots1.py

--- 2022-03-05-evaluating-ms-playwright-for-gray-hat-automation/screenshots1.py
+++ 2022-03-05-evaluating-ms-playwright-for-gray-hat-automation/screenshots1.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     for url in ["https://www.stockx.com", "https://ifconfig.me"]:
         for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser = p.chromium.launch()
+            browser = browser_type.launch()
             page = browser.new_page()
             # page = browser.new_page("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36")
             page.goto(url)
