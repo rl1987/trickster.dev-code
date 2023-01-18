7d10d6324188f4b501c78a1ea3333510879c5e6b
rl1987 <rl1987@users.noreply.github.com>
2023-01-18 23:05:08 +0800

    Add few more files

---
+++ 2022-10-08-javascript-ast-manipulation-with-babel-defeating-string-array-mapping/clean.js
@@ -0,0 +1,4 @@
+var _0x3baf = ["Hello Venus", "log", "Hello Earth", "Hello Mars"];
+console["log"]("Hello Venus");
+console["log"]("Hello Earth");
+console["log"]("Hello Mars");
---
+++ 2022-10-08-javascript-ast-manipulation-with-babel-defeating-string-array-mapping/clean2.js
@@ -0,0 +1,20 @@
+var _0x64a2 = ["https://hackerone.com/hacktivity", "hackerone.com", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "en-GB,en-US;q=0.9,en;q=0.8", "max-age=0", "_dd_s=", "https://hackerone.com/", "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"", "?0", "\"macOS\"", "document", "navigate", "same-origin", "?1", "1", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"];
+fetch("https://hackerone.com/hacktivity", {
+  headers: {
+    'authority': "hackerone.com",
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    'accept-language': "en-GB,en-US;q=0.9,en;q=0.8",
+    'cache-control': "max-age=0",
+    'cookie': "_dd_s=",
+    'referer': "https://hackerone.com/",
+    'sec-ch-ua': "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
+    'sec-ch-ua-mobile': "?0",
+    'sec-ch-ua-platform': "\"macOS\"",
+    'sec-fetch-dest': "document",
+    'sec-fetch-mode': "navigate",
+    'sec-fetch-site': "same-origin",
+    'sec-fetch-user': "?1",
+    'upgrade-insecure-requests': "1",
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+  }
+});
