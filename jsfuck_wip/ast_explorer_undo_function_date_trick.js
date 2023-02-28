1a202ac463153cd3ff03b48551ff7832e242c2c8
rl1987 <rl1987@users.noreply.github.com>
2023-02-28 19:54:28 +0800

    Undo couple of hacks that leverage Function()

---
+++ jsfuck_wip/ast_explorer_undo_function_date_trick.js
@@ -0,0 +1,22 @@
+export default function (babel) {
+  const { types: t } = babel;
+
+  return {
+    name: "ast-transform", // not required
+    visitor: {
+      CallExpression(path) {
+        let node = path.node;
+        if (t.isCallExpression(node.callee) &&
+            t.isCallExpression(node.callee.callee) &&
+            t.isIdentifier(node.callee.callee.callee) &&
+            node.callee.callee.callee.name === "Function" &&
+            node.callee.callee.arguments.length === 1 &&
+            t.isStringLiteral(node.callee.callee.arguments[0]) &&
+            node.callee.callee.arguments[0].value === "return Date"
+           ) {
+            path.replaceWith(t.valueToNode(Date()));
+        }
+      }
+    }
+  };
+}
---
+++ jsfuck_wip/ast_explorer_undo_function_escape_trick.js
@@ -0,0 +1,23 @@
+export default function (babel) {
+  const { types: t } = babel;
+
+  return {
+    name: "ast-transform", // not required
+    visitor: {
+      CallExpression(path) {
+        let node = path.node;
+        if (t.isCallExpression(node.callee) &&
+            t.isCallExpression(node.callee.callee) &&
+            t.isIdentifier(node.callee.callee.callee) &&
+            node.callee.callee.callee.name === "Function" &&
+            node.callee.callee.arguments.length === 1 &&
+            t.isStringLiteral(node.callee.callee.arguments[0]) &&
+            node.callee.callee.arguments[0].value === "return escape" &&
+            t.isStringLiteral(node.arguments[0])
+           ) {
+            path.replaceWith(t.valueToNode(escape(node.arguments[0].value)));
+        }
+      }
+    }
+  };
+}
