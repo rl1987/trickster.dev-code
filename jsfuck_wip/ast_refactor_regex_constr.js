c44e1e4c811b29db11e067d7af86b4590c7c9a54
rl1987 <rl1987@users.noreply.github.com>
2023-05-27 19:23:43 +0300

    Further undo regex tricks
    This changes code from:
    
    /false/["constructor"]("/")
    
    to:
    
    /\//

---
+++ jsfuck_wip/ast_refactor_regex_constr.js
@@ -0,0 +1,22 @@
+export default function (babel) {
+  const { types: t } = babel;
+
+  return {
+    name: "refactor-regex-constr", // not required
+    visitor: {
+      CallExpression(path) {
+        const callExpr = path.node;
+        if (callExpr.arguments.length != 1) return;
+        const argument = callExpr.arguments[0];
+        if (!t.isStringLiteral(argument)) return;
+        const calleeMemberExpr = callExpr.callee;
+        if (!t.isMemberExpression(calleeMemberExpr)) return;
+        if (!t.isRegExpLiteral(calleeMemberExpr.object)) return;
+        let property = calleeMemberExpr.property;
+        if (!t.isStringLiteral(property)) return;
+        if (property.value != "constructor") return;
+        
+        path.replaceWith(t.regExpLiteral(argument.value.replace('/', '\\/'), ''));
+    }
+  };
+}
