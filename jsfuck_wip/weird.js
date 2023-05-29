90d1eb14d7359e26d3982192f47ab5910df63465
rl1987 <rl1987@users.noreply.github.com>
2023-05-29 20:01:30 +0300

    Note a weird behaviour of JS engine on one of the intermediate step
    The difference between first and second statement is that undo-entries-trick
    was applied.

---
+++ jsfuck_wip/weird.js
@@ -0,0 +1,53 @@
+eval(
+[]["flat"]["constructor"]("return" +
+    "<font color=\"undefined\"></font>" [
+        12
+    ] +
+    "t40t143t157nst157le.lt157t147t50t42t100t110t112t113t101t42t51" [
+        "s" + 211["to" + "" [
+            "constructor"
+        ]["na" + (0["constructor"] +
+            [])[11] + "e"]]("31")[1] +
+        "l" + "i" + "t"
+    ]("t")[([]["entries"]() + [])[3] +
+        "o" + "i" + "n"](([]["flat"][
+        "constructor"
+    ]("return" + "false0" [
+            "italics"
+        ]()[10] + false +
+        "false0" ["italics"]()[
+            10])()[
+        "constructor"](
+        "false0" ["italics"]()[
+            10]) + [])[1]) +
+    "<font color=\"undefined\"></font>" [
+        12
+    ])()
+);
+
+eval(
+[]["flat"]["constructor"]("return" +
+    "<font color=\"undefined\"></font>" [
+        12
+    ] +
+    "t40t143t157nst157le.lt157t147t50t42t100t110t112t113t101t42t51" [
+        "s" + 211["to" + "" [
+            "constructor"
+        ]["na" + (0["constructor"] +
+            [])[11] + "e"]]("31")[1] +
+        "l" + "i" + "t"
+    ]("t")[("function entries() { [native code] }" + [])[3] + 
+        "o" + "i" + "n"](([]["flat"][
+        "constructor"
+    ]("return" + "false0" [
+           "italics"
+        ]()[10] + false +
+        "false0" ["italics"]()[
+            10])()[
+        "constructor"](
+        "false0" ["italics"]()[
+            10]) + [])[1]) +
+    "<font color=\"undefined\"></font>" [
+        12
+    ])()
+);
