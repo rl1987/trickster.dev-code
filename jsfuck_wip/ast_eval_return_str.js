8423f881a6215f5518ba92b216d38936ac78f3d3
rl1987 <rl1987@users.noreply.github.com>
2023-06-02 17:37:18 +0300

    Tweak decodeUnicodeChar()

--- jsfuck_wip/ast_eval_return_str.js
+++ jsfuck_wip/ast_eval_return_str.js
@@ -1,3 +1,17 @@
+// Based on code from: https://stackoverflow.com/a/62633125
+function decodeUnicodeChar(str) {
+  if (str == null) return ""; // no need extra test on undefined
+  var r = /\\u([\d\w]{4})/gi;
+  var r3 = /\\([\d\w]{3})/gi;
+  var r2 = /\\([\d\w]{2})/gi;
+
+  str = str.replace(r, (match, grp) => String.fromCharCode(parseInt(grp, 16)))
+           .replace(r3, (match, grp) => String.fromCharCode(parseInt(grp, 8)))
+           .replace(r2, (match, grp) => String.fromCharCode(parseInt(grp, 8)))
+           .replace(/\n/g, "<br>");
+  return str; // return it
+}
+
 export default function (babel) {
   const { types: t } = babel;
 
@@ -16,9 +30,7 @@ export default function (babel) {
         if (argStr.startsWith("return\"") && argStr.endsWith("\"")) {
           argStr = argStr.substr("return\"".length);
           argStr = argStr.substr(0, argStr.length-1);
-          // HACK to deal with escaped characters.
-          // https://stackoverflow.com/questions/48030856/evaluating-escaped-string-literals-in-javascript
-          argStr = eval('"' + argStr + '"')
+          argStr = decodeUnicodeChar(argStr);
           path.replaceWith(t.stringLiteral(argStr));
         }
       }
