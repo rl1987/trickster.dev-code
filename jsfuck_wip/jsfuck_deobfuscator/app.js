0be25f7e2c199d65414146419acf28c6dcee8650
rl1987 <rl1987@users.noreply.github.com>
2023-06-03 15:00:36 +0300

    Transform the code until it cannot be changed any further

--- jsfuck_wip/jsfuck_deobfuscator/app.js
+++ jsfuck_wip/jsfuck_deobfuscator/app.js
@@ -5,11 +5,24 @@ const babel = require("@babel/core");
 const t = require("@babel/types");
 
 // TODO: take file paths via CLI
-const result = babel.transformFileSync(path.join('./input.js') , {
-    plugins: [
-      // TODO: turn each AST transform into proper Babel plugin
-      path.join(__dirname, 'plugin_eval_expr.js')
-    ]
-});
+let js = fs.readFileSync("./input.js", "utf-8");
+console.log(js);
+console.log("-----------------------------------------------------------------");
 
-fs.writeFileSync(path.join(__dirname, 'output.js'), result.code, 'utf-8');
+while (true) {
+  const result = babel.transformSync(js, {
+      plugins: [
+        // TODO: turn each AST transform into proper Babel plugin
+        path.join(__dirname, 'plugin_eval_expr.js')
+      ]
+  });
+
+  if (result.code == js) break;
+
+  console.log(result.code);
+  console.log("-----------------------------------------------------------------");
+
+  js = result.code;
+}
+
+fs.writeFileSync(path.join(__dirname, 'output.js'), js, 'utf-8');
