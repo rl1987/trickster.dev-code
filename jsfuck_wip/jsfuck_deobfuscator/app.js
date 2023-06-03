1e215fbf278b4bb20307dd1080fecc2de36f7eb3
rl1987 <rl1987@users.noreply.github.com>
2023-06-03 14:50:35 +0300

    Convert expression evaluation code from part 1 into importable Babel plugin

--- jsfuck_wip/jsfuck_deobfuscator/app.js
+++ jsfuck_wip/jsfuck_deobfuscator/app.js
@@ -5,12 +5,11 @@ const babel = require("@babel/core");
 const t = require("@babel/types");
 
 // TODO: take file paths via CLI
-const result = babel.transformFileSync(path.join(__dirname, 'input.js') , {
+const result = babel.transformFileSync(path.join('./input.js') , {
     plugins: [
       // TODO: turn each AST transform into proper Babel plugin
+      path.join(__dirname, 'plugin_eval_expr.js')
     ]
 });
-
-console.log(result.code);
 
 fs.writeFileSync(path.join(__dirname, 'output.js'), result.code, 'utf-8');
---
+++ jsfuck_wip/jsfuck_deobfuscator/plugin_eval_expr.js
@@ -0,0 +1,61 @@
+const babel = require("@babel/core");
+const t = require("@babel/types");
+
+module.exports = function (babel) {
+  return {
+    name: "eval-expr", // not required
+    visitor: {
+      "UnaryExpression|BinaryExpression"(path) {
+        let node = path.node;
+        
+        if (t.isNumericLiteral(node.left) && t.isNumericLiteral(node.right)) {
+          if (node.right.value === 0) {
+           	if (node.left.value === 1) {
+              path.replaceWith(t.identifier('Infinity'));
+              return;
+            } else if (node.left.value === 0) {
+              path.replaceWith(t.identifier('NaN'));
+              return;
+            }
+          }
+        }
+
+        let result = path.evaluate();
+        if (result.confident) {
+          let valueNode = t.valueToNode(result.value);
+          if (t.isLiteral(valueNode)) {
+            path.replaceWith(valueNode);
+          } else {
+            path.replaceWith(valueNode);
+            path.skip();
+          }
+        }
+      },
+      MemberExpression(path) {
+      	let node = path.node;
+        
+        // https://stackoverflow.com/a/52986361/1044147
+        function isNumeric(n) {
+  	      return !isNaN(parseFloat(n)) && isFinite(n);
+        }
+          
+        if (isNumeric(node.property.value)) {
+          node.property = t.valueToNode(Number(node.property.value));
+        }
+        
+      	if (t.isStringLiteral(node.object) && 
+            t.isNumericLiteral(node.property)) {
+      	  let character = node.object.value[node.property.value];
+      	  path.replaceWith(t.valueToNode(character));
+        }
+        
+        if (t.isArrayExpression(node.object) &&
+            node.object.elements.length == 0 &&
+            t.isArrayExpression(node.property) &&
+            node.property.elements.length == 0) {
+          path.replaceWith(t.valueToNode(undefined));
+        }
+      }
+    }
+  };
+}
