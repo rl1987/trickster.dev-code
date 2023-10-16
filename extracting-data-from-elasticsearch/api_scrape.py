a215516c9f854f66186207dcd76c16b1de41b60e
rl1987 <rl1987@users.noreply.github.com>
2023-10-16 17:23:43 +0300

    Run black to fix formatting

--- extracting-data-from-elasticsearch/api_scrape.py
+++ extracting-data-from-elasticsearch/api_scrape.py
@@ -10,39 +10,41 @@ import requests
 USERNAME = "collections"
 PASSWORD = "o41KG!MmJQ$A66"
 
+
 def create_session():
     session = requests.Session()
 
     session.headers = {
-        'authority': 'collection.carnegieart.org',
-        'accept': 'application/json',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'cache-control': 'no-cache',
-        'content-type': 'application/json',
-        'origin': 'https://collection.carnegieart.org',
-        'pragma': 'no-cache',
-        'referer': 'https://collection.carnegieart.org/',
-        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        "authority": "collection.carnegieart.org",
+        "accept": "application/json",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "origin": "https://collection.carnegieart.org",
+        "pragma": "no-cache",
+        "referer": "https://collection.carnegieart.org/",
+        "sec-ch-ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
     }
 
     session.auth = (USERNAME, PASSWORD)
 
     return session
 
+
 def gen_rows_from_response_payload(resp_json):
-    hits = resp_json.get('hits', dict()).get('hits', [])
+    hits = resp_json.get("hits", dict()).get("hits", [])
 
     for hit_dict in hits:
         source_dict = hit_dict.get("_source", dict())
 
         item = dict()
-        
+
         item["title"] = source_dict.get("title")
         item["department"] = source_dict.get("department")
         item["category"] = source_dict.get("type")
@@ -71,7 +73,9 @@ def gen_rows_from_response_payload(resp_json):
             item["maker_gender"] = "|".join(maker_genders).replace("None", "")
 
         try:
-            item["acquired_year"] = source_dict.get("acquisition_date", "").split("-")[0]
+            item["acquired_year"] = source_dict.get("acquisition_date", "").split("-")[
+                0
+            ]
         except:
             pass
 
@@ -79,65 +83,65 @@ def gen_rows_from_response_payload(resp_json):
         item["accession_number"] = source_dict.get("accession_number")
         item["credit_line"] = source_dict.get("credit_line")
 
-        item["url"] = "https://collection.carnegieart.org/objects/" + source_dict.get("id", "").replace("cmoa:objects/", "")
-    
+        item["url"] = "https://collection.carnegieart.org/objects/" + source_dict.get(
+            "id", ""
+        ).replace("cmoa:objects/", "")
+
         yield item
+
 
 def scrape(session):
     json_payload = {
-        "query": {
-            "match_all": dict()
-        },
+        "query": {"match_all": dict()},
         "size": 24,
         "from": 0,
-        "sort": [
-            {
-                "acquisition_date": {
-                    "order": "desc"
-                }
-            }
-        ],
+        "sort": [{"acquisition_date": {"order": "desc"}}],
     }
 
-    params = {
-        "scroll": "1m"
-    }
+    params = {"scroll": "1m"}
 
-    resp0 = session.post("https://collection.carnegieart.org/api/cmoa_objects/_search",
-                         json=json_payload, params=params)
+    resp0 = session.post(
+        "https://collection.carnegieart.org/api/cmoa_objects/_search",
+        json=json_payload,
+        params=params,
+    )
     print(resp0.url)
 
     yield from gen_rows_from_response_payload(resp0.json())
 
-    scroll_id = resp0.json().get('_scroll_id')
+    scroll_id = resp0.json().get("_scroll_id")
 
     while scroll_id is not None:
-        resp = session.get("https://collection.carnegieart.org/api/_search/scroll/" + scroll_id,
-                           params=params)
+        resp = session.get(
+            "https://collection.carnegieart.org/api/_search/scroll/" + scroll_id,
+            params=params,
+        )
         print(resp.url)
 
         yield from gen_rows_from_response_payload(resp.json())
-        hits = resp.json().get('hits', dict()).get('hits', [])
+        hits = resp.json().get("hits", dict()).get("hits", [])
         if len(hits) == 0:
             break
 
-        scroll_id = resp.json().get('_scroll_id')
+        scroll_id = resp.json().get("_scroll_id")
+
 
 def main():
     session = create_session()
- 
+
     out_f = open("carnegie.csv", "w", encoding="utf-8")
     csv_writer = None
 
     for row in scrape(session):
         print(row)
         if csv_writer is None:
-            csv_writer = csv.DictWriter(out_f, fieldnames=list(row.keys()),
-                                        lineterminator="\n")
+            csv_writer = csv.DictWriter(
+                out_f, fieldnames=list(row.keys()), lineterminator="\n"
+            )
             csv_writer.writeheader()
 
         csv_writer.writerow(row)
 
+
 if __name__ == "__main__":
     main()
-
