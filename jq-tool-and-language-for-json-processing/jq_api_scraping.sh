1bbcccdf7c8d0394dd05354502c0640c68fa76c2
rl1987 <rl1987@users.noreply.github.com>
2025-06-10 20:47:08 +0300

    Refrain from strong JSON strings in files

--- jq-tool-and-language-for-json-processing/jq_api_scraping.sh
+++ jq-tool-and-language-for-json-processing/jq_api_scraping.sh
@@ -9,13 +9,11 @@ PAGE=1
 echo "id,title,body_html,vendor,product_type,price,handle" > hbb.csv
 
 while true ; do
-    curl "$URL?page=$PAGE&limit=$PER_PAGE" > "$PAGE.json"
-    jq -r '.products[] | [.id, .title, .body_html, .vendor, .product_type, .variants[0].price, .handle] | @csv' "$PAGE.json" >> hbb.csv
-    N_PRODUCTS=$(jq -r '.products | length' "$PAGE.json")
+    JSON_STR=$(curl "$URL?page=$PAGE&limit=$PER_PAGE")
+    echo "$JSON_STR" | jq -r '.products[] | [.id, .title, .body_html, .vendor, .product_type, .variants[0].price, .handle] | @csv' >> hbb.csv
+    N_PRODUCTS=$(echo "$JSON_STR" | jq -r '.products | length')
     if [[ "$N_PRODUCTS" -lt "$PER_PAGE" ]]; then
         break
     fi
     PAGE=$((PAGE+1))
 done
-
-rm *.json
