6c198a6065956f79677d203e34e114a6dc3c4333
rl1987 <rl1987@users.noreply.github.com>
2025-10-29 19:58:00 +0700

    Add njsparser test snippet

---
+++ 2025-10-31-scraping-nextjs-sites-in-2025/njsparser_test.py
@@ -0,0 +1,22 @@
+import requests
+import njsparser
+from pprint import pprint
+
+proxy_url = input("Proxy URL: ")
+
+if proxy_url.strip == "":
+    proxy_url = None
+
+proxies = {
+    "https": proxy_url
+}
+
+url = input("Page URL: ")
+
+response = requests.get(url, proxies, verify=False)
+print(response.url, response.status_code)
+
+fd = njsparser.BeautifulFD(response.text)
+for data in fd.find_iter([njsparser.T.Data, njsparser.T.Element]):
+    print(type(data))
+    pprint(data)
